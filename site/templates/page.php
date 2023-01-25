74995281c99635b6e294925cde8892f062f727bc
Giulia Polenta <giuliapolenta@gmail.com>
2023-01-25 12:04:22 +0100

    Add page template

---
+++ site/templates/page.php
@@ -0,0 +1,10 @@
+<?= snippet('header') ?>
+<?= snippet('sidebar') ?>
+<main class="main">
+<?= snippet('topbar') ?>
+
+<?php foreach ($page->children()->listed() as $part) : ?>
+    <?php snippet($part->intendedTemplate(), compact('part')) ?>
+<?php endforeach ?>
+
+<?= snippet('footer') ?>
