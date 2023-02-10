a75440ad935880e77022433b4cb3beef5a661a63
Giulia Polenta <giuliapolenta@gmail.com>
2023-02-10 14:50:12 +0100

    Fix text formatting issues with smartypants option

--- site/config/config.php
+++ site/config/config.php
@@ -1,11 +1,16 @@
 <?php
 
 return [
-    'markdown' => [
-      'breaks' => true
-    ]
-];
-
-return [
-  'languages' => true
+    'languages' => true,
+    'smartypants' => [
+      'space' => '(?: | |&nbsp;|&#0*160;|&#x0*[aA]0;)',
+      'space.emdash' => ' ',
+      'space.endash' => ' ',
+      'space.colon' => '&#160;',
+      'space.semicolon' => '&#160;',
+      'space.marks' => '&#160;',
+      'space.frenchquote' => '&#160;',
+      'space.thousand' => '&#160;',
+      'space.unit' => '&#160;',
+    ],
 ];
